3cb779cefb33ef0c349e32956905c95e69150050
Shyam Kumar <shyamkumar@vajro.com>
2022-02-07 15:42:32 +0530

    Initial Commit

---
+++ NXT-LoginModule/NXT_LoginModule.h
@@ -0,0 +1,18 @@
+//
+//  NXT_LoginModule.h
+//  NXT-LoginModule
+//
+//  Created by Shyam Kumar on 07/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NXT_LoginModule.
+FOUNDATION_EXPORT double NXT_LoginModuleVersionNumber;
+
+//! Project version string for NXT_LoginModule.
+FOUNDATION_EXPORT const unsigned char NXT_LoginModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NXT_LoginModule/PublicHeader.h>
+
+
